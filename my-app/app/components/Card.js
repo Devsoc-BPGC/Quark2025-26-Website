2e33b05884dc241bbf3039cfef778a7cc236007d
Vaibhav Kumar Srivastava <kumar.vaibhav6122005@gmail.com>
2026-01-20 23:24:18 +0530

    implement spin animation

--- my-app/app/components/Card.js
+++ my-app/app/components/Card.js
@@ -1,9 +1,8 @@
-
+import Image from "next/image"
 export default function Card() {
     return (
-        <div className="w-67 h-90 rounded-[9px] bg-[#3B267875] border-2 border-[#7700FF]
-            skew-6
-        ">
+        <div className="w-42 aspect-[0.744] inline-block rounded-[9px] bg-[#3B267875] border-2 border-[#7700FF] relative">
+            {/* <Image src={'image 27.svg'} fill alt="img" /> */}
         </div>
     )
 }
--- my-app/app/gallery/page.js
+++ my-app/app/gallery/page.js
@@ -1,7 +1,19 @@
+"use client"
 import Image from "next/image";
 import Card from "../components/Card";
+import { useState, useEffect } from "react";
 
 export default function Gallery() {
+    const [rotation, setRotation] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRotation(prev => prev + 0.15);
+        }, 16);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="h-screen">
             <div className="h-screen flex flex-col justify-end">
@@ -22,10 +34,35 @@ export default function Gallery() {
                 </div>
             </div>
 
-            <div>
-                <Card />
+            <div className="font-(family-name:--font-orbitron) font-extrabold text-8xl absolute bottom-0 p-3">
+                GALLERY
             </div>
 
+            <div className="absolute bottom-0 w-screen h-screen
+                perspective-[1000px] transform-3d flex justify-center items-center transform -rotate-x-23
+                pb-85
+            ">
+                <div 
+                    className="relative w-0 h-0"
+                    style={{ 
+                        transformStyle: 'preserve-3d',
+                        transform: `rotateY(${rotation}deg)`
+                    }}
+                >
+                    {[0,1,2,3,4,5,6,7,8].map(i => (
+                        <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+                            key={i}
+                            style={{
+                                transformStyle: 'preserve-3d',
+                                transform: `rotateY(${i * 40}deg) rotateX(-5deg) translateZ(450px)`
+                            }}
+                        >
+                            <Card />
+                        </span>
+                    ))}
+                </div>
+            </div>
         </div>
+
     )
 }
